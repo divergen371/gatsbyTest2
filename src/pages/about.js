971575065971e1547b14db0f395673512e4cf76a
divergen371 <divergen@protonmail.com>
2021-07-18 13:20:49 +0900

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,21 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
+import Layout from "../components/Layout";
+import * as styles from "./about.module.css";
 
 const AboutPage = (props) => {
   return (
-    <div>
+    <Layout>
       <Helmet>
         <title>My Gatsby Site</title>
         <meta name="description" content="My first gatsby website" />
       </Helmet>
-      <p>Hello Gatsby</p>
-    </div>
+      <h2 className={styles.name}>@mottox2</h2>
+      <p className={styles.text}>
+        都内でウェブサービスやスマホアプリを作るフロントエンドエンジニア・UIデザイナーです。
+        新規事業の爆速立ち上げや、使いやすいSPAの開発が得意です。
+      </p>
+    </Layout>
   );
 };
 
